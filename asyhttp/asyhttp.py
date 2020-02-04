c95172357490c01f5b9d5dec900711cfc2f39d40
pyno <pyno@pwnpad>
2020-02-04 13:00:37 +0100

    Multiple improvements
    Function loop()
    	- Modified function prototype to avoid "obscure" **kwargs and simplified the argument management
    	- Added max_concurrent parameter in order to allow users to tune the maximum number of concurrent requests made by the async loop
    	- Added usrdata parameter in order to allow users to pass context specific data to the process_out function (see dirfy for an example)
    
    Function _run_task()
    	- Updated function prototype and added code to handle max_concurrent and usrdata parameters
    	- Added code to manage remote dns resolution when connecting to a proxy
    
    Function _bound_fetch()
    	- Updated function prototype and code to handle usrdata parameter
    	- Removed proxy_str since it is now included in the ClientSession object (which handles remote dns resolution)
    
    Function _fetch()
    	- Updated function prototype and code to handle usrdata parameter
    	- Removed proxy_str since it is now included in the ClientSession object (which handles remote dns resolution)
    
    Function _aGET()
    	- Removed proxy_str since it is now included in the ClientSession object (which handles remote dns resolution)
    
    Function _aPOST()
    	- Removed proxy_str since it is now included in the ClientSession object (which handles remote dns resolution)
    
    Function _aHEAD()
    	- Removed proxy_str since it is now included in the ClientSession object (which handles remote dns resolution)

--- asyhttp/asyhttp.py
+++ asyhttp/asyhttp.py
@@ -9,88 +9,92 @@
 #
 
 import asyncio
-from aiohttp import ClientSession, TCPConnector
+from aiohttp import ClientSession, TCPConnector, client_exceptions
+from aiohttp_proxy import ProxyConnector, ProxyType
 import sys
 
 version = "0.1"
 
-def loop(**kwargs):
-	try:
-		req_dict_list = kwargs["urls"]
-	except:
-		sys.stdout.write(" [!] No URL list in loop.\n [!] Bye!\n")
-		exit(1)
-	def process_output(url,return_code,reason,resp_body):
-		sys.stdout.write(" [>] "+str(return_code)+" : "+url+"\n")
-	if "process_out" in kwargs:
-		process_output = kwargs["process_out"]
-	proxy_str = ""
-	if "proxy" in kwargs:
-		proxy_str = kwargs["proxy"]
-	verify_tls = False
-	if "verify_tls" in kwargs:
-		verify_tls = kwargs["verify_tls"]
-	redirects = False
-	if "redirects" in kwargs:
-		redirects = kwargs["redirects"]
+def loop(urls, process_out=None, proxy='', verify_tls=False, redirects=False, max_concurrent=1000, usrdata={}):
+    if len(urls) < 1:
+        sys.stdout.write(" [!] No URL list in loop.\n [!] Bye!\n")
+        exit(1)
 
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(_run_tasks(req_dict_list,proxy_str,process_output,verify_tls,redirects))
+    if process_out:
+        process_output = process_out
+    else:
+        def process_output(url,return_code,reason,resp_body):
+            sys.stdout.write(" [>] "+str(return_code)+" : "+url+"\n")
 
-async def _run_tasks(req_dict_list,proxy_str,process_output,verify_tls,redirects):
-	tasks = []
-	sem = asyncio.Semaphore(1000)
-	conn = TCPConnector(ttl_dns_cache=None)	# Number of secs, None means cached forever
-	async with ClientSession(connector=conn) as client:
-		for req_dict in req_dict_list:
-			# pyhton 3.7
-			# tasks.append(asyncio.create_task(_bound_fetch(...)))
-			# -----------------
-			# tasks.append(asyncio.ensure_future(_bound_fetch(...)))
-			tasks.append(_bound_fetch(sem,req_dict,client,proxy_str,process_output,verify_tls,redirects))
-		await asyncio.gather(*tasks)
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(_run_tasks(urls, proxy, max_concurrent, process_output, verify_tls, redirects, usrdata))
+    except client_exceptions.ServerDisconnectedError as e:
+        print("\n [!] Error ServerDisconnectedError - try lowering max_concurrent value")
 
-async def _bound_fetch(sem, req_dict, client, proxy_str, process_output,verify_tls,redirects):
-	async with sem:
-		await _fetch(req_dict, client, proxy_str, process_output,verify_tls,redirects)
+async def _run_tasks(req_dict_list, proxy_str, max_concurrent, process_output, verify_tls, redirects, usrdata):
+    tasks = []
+    sem = asyncio.Semaphore(max_concurrent)
 
-async def _fetch(req_dict,client,proxy_str,process_output,verify_tls,redirects):
-	if not "method" in req_dict:
-		sys.stdout.write(" [!] Missing method in URL dict: {}\n".format(req_dict))
-		return
-	elif not "url" in req_dict:
-		sys.stdout.write(" [!] Missing url in URL dict: {}\n".format(req_dict))
-		return
+    conn = None
+    if proxy_str != '':
+        # ============== handle remote dns resolution =======================
+        p = int(proxy_str.split(':')[2])
+        h = proxy_str.split('://')[1].split(':')[0]
+        conn = ProxyConnector(proxy_type=ProxyType.HTTP, host=h, port=p, rdns=True)
+    else:
+        conn = TCPConnector(ttl_dns_cache=None) # Number of secs, None means cached forever
 
-	headers = {}
-	if "headers" in req_dict:
-		headers = req_dict["headers"]
+    async with ClientSession(connector=conn) as client:
+        for req_dict in req_dict_list:
+            # pyhton 3.7
+            # tasks.append(asyncio.create_task(_bound_fetch(...)))
+            # -----------------
+            # tasks.append(asyncio.ensure_future(_bound_fetch(...)))
+            tasks.append(_bound_fetch(sem,req_dict, client, process_output, verify_tls, redirects, usrdata))
+        await asyncio.gather(*tasks)
 
-	if req_dict["method"] == "GET":
-		resp_body,return_code,reason = await _aGET(req_dict["url"],headers,client,proxy_str,verify_tls,redirects)
-	elif req_dict["method"] == "HEAD":
-		resp_body,return_code,reason = await _aHEAD(req_dict["url"],headers,client,proxy_str,verify_tls,redirects)
-	elif req_dict["method"] == "POST":
-		resp_body,return_code,reason = await _aPOST(req_dict["url"],headers,req_dict["body"],client,proxy_str,verify_tls,redirects)
-	else:
-		sys.stdout.write(" [!] HTTP method not supported: {}\n".format(req_dict["method"]))
-		return
-	process_output(req_dict["url"], return_code, reason, resp_body)
+async def _bound_fetch(sem, req_dict, client, process_output,verify_tls, redirects, usrdata):
+    async with sem:
+        await _fetch(req_dict,client,process_output,verify_tls,redirects,usrdata)
 
-async def _aGET(url,headers,client,proxy_str,verify_tls,redirects):
-	async with client.get(url, headers=headers, proxy=proxy_str, verify_ssl=verify_tls, allow_redirects=redirects) as response:
-		return_code = response.status
-		reason = response.reason
-		return await response.content.read(),return_code,reason
+async def _fetch(req_dict,client,process_output,verify_tls,redirects,usrdata):
+    if not "method" in req_dict:
+        sys.stdout.write(" [!] Missing method in URL dict: {}\n".format(req_dict))
+        return
+    elif not "url" in req_dict:
+        sys.stdout.write(" [!] Missing url in URL dict: {}\n".format(req_dict))
+        return
 
-async def _aPOST(url,headers,body,client,proxy_str,verify_tls,redirects):
-	async with client.post(url, headers=headers, data=body, proxy=proxy_str, verify_ssl=verify_tls, allow_redirects=redirects) as response:
-		return_code = response.status
-		reason = response.reason
-		return await response.content.read(),return_code,reason
+    headers = {}
+    if "headers" in req_dict:
+        headers = req_dict["headers"]
 
-async def _aHEAD(url,headers,client,proxy_str,verify_tls,redirects):
-	async with client.head(url, headers=headers, proxy=proxy_str, verify_ssl=verify_tls, allow_redirects=redirects) as response:
-		return_code = response.status
-		reason = response.reason
-		return await response.content.read(),return_code,reason
+    if req_dict["method"] == "GET":
+        resp_body,return_code,reason = await _aGET(req_dict["url"],headers,client,verify_tls,redirects)
+    elif req_dict["method"] == "HEAD":
+        resp_body,return_code,reason = await _aHEAD(req_dict["url"],headers,client,verify_tls,redirects)
+    elif req_dict["method"] == "POST":
+        resp_body,return_code,reason = await _aPOST(req_dict["url"],headers,req_dict["body"],client,verify_tls,redirects)
+    else:
+        sys.stdout.write(" [!] HTTP method not supported: {}\n".format(req_dict["method"]))
+        return
+    process_output(req_dict["url"], return_code, reason, resp_body, usrdata)
+
+async def _aGET(url,headers,client,verify_tls,redirects):
+    async with client.get(url, headers=headers, verify_ssl=verify_tls, allow_redirects=redirects) as response:
+        return_code = response.status
+        reason = response.reason
+        return await response.content.read(),return_code,reason
+
+async def _aPOST(url,headers,body,client,verify_tls,redirects):
+    async with client.post(url, headers=headers, data=body, verify_ssl=verify_tls, allow_redirects=redirects) as response:
+        return_code = response.status
+        reason = response.reason
+        return await response.content.read(),return_code,reason
+
+async def _aHEAD(url,headers,client,verify_tls,redirects):
+    async with client.head(url, headers=headers, verify_ssl=verify_tls, allow_redirects=redirects) as response:
+        return_code = response.status
+        reason = response.reason
+        return await response.content.read(),return_code,reason
